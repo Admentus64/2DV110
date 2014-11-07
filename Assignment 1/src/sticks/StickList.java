af0f0031df371a0661acff1c62e2faf31b8fcaee
Robert Hallink <rh222ff@student.lnu.se>
2014-11-07 17:23:31 +0100

    GREEN

--- Assignment 1/src/sticks/StickList.java
+++ Assignment 1/src/sticks/StickList.java
@@ -1,9 +1,16 @@
 package sticks;
+import java.util.ArrayList;
 
 public class StickList {
 	
+	private ArrayList<Stick> sticks;
+	
 	public StickList() {
-		
+		sticks = new ArrayList<Stick>();
+	}
+	
+	public void add(Stick stick) {
+		sticks.add(stick);
 	}
 	
 }
