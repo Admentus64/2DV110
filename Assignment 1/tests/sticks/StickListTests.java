f5255320d62cbc6c989a89cb87823c155cae3070
Robert Hallink <rh222ff@student.lnu.se>
2014-11-07 17:18:20 +0100

    RED

---
+++ Assignment 1/tests/sticks/StickListTests.java
@@ -0,0 +1,12 @@
+package sticks;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class StickListTests {
+	
+	@Test
+	public void shouldCreateEmptyInstance() {
+		new StickList();
+	}
+	
+}
