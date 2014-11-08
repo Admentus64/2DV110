1c69ec9708413ab533881b8b4463ad6ed75bcf76
Robert Hallink <rh222ff@student.lnu.se>
2014-11-08 13:04:53 +0100

    RED

--- Assignment 1/tests/sticks/StickListTests.java
+++ Assignment 1/tests/sticks/StickListTests.java
@@ -14,6 +14,25 @@ public class StickListTests {
 		createStickList(30);
 	}
 	
+	@Test
+	public void shouldRemoveStickWithoutError() {
+		StickList list = createStickList(30);
+		list.remove(0);
+		
+	}
+	
+	@Test
+	public void shouldRemoveStickWithError() {
+		StickList list = createStickList(30);
+		int error = 0;
+		try					{ list.remove(-1); }
+		catch(Exception e)	{ error++; }
+		try					{ list.remove(31); }
+		catch(Exception e)	{ error++; }
+		if (error != 2)
+			fail("Failed test");
+	}
+	
 	
 	
 	private StickList createStickList(int size) {
