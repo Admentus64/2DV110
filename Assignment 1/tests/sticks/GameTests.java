576d0a61ba123202006a4415119b0b617cdd77d2
Robert Hallink <rh222ff@student.lnu.se>
2014-11-11 14:02:17 +0100

    GREEN

--- Assignment 1/tests/sticks/GameTests.java
+++ Assignment 1/tests/sticks/GameTests.java
@@ -6,7 +6,16 @@ public class GameTests {
 	
 	@Test
 	public void shouldCreateInstance() {
-		new Game();
+		Game game = initGame();
+	}
+	
+	
+	
+	private Game initGame() {
+		StickList list = new StickList();
+		for (int i=0; i<30; i++)
+			list.add(new Stick());
+		return new Game(list, new Player(), new AI());
 	}
 	
 }
