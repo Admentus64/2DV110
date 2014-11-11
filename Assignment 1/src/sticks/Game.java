dcbbffd8113388c18a01d3a13e769cb7056a5c37
Robert Hallink <rh222ff@student.lnu.se>
2014-11-11 13:57:54 +0100

    RED

--- Assignment 1/src/sticks/Game.java
+++ Assignment 1/src/sticks/Game.java
@@ -2,10 +2,14 @@ package sticks;
 
 public class Game {
 	
-	public Game() {
-		
+	private StickList list;
+	private Player user;
+	private Player pc;
+	
+	public Game(StickList list, Player user, Player pc) {
+		this.list = list;
+		this.user = user;
+		this.pc = pc;
 	}
-	
-	
 	
 }
