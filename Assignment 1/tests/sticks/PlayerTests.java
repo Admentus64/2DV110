3b6bad65e1fd8cd11411371d47e7491943a6596a
Robert Hallink <rh222ff@student.lnu.se>
2014-11-13 22:04:55 +0100

    RED

--- Assignment 1/tests/sticks/PlayerTests.java
+++ Assignment 1/tests/sticks/PlayerTests.java
@@ -1,4 +1,5 @@
 package sticks;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class PlayerTests {
@@ -6,6 +7,14 @@ public class PlayerTests {
 	@Test
 	public void shouldCreateInstance() {
 		new Player();
+	}
+	
+	@Test
+	public void shouldInputNumber() {
+		Player player = new Player();
+		int i = player.drawSticks();
+		System.out.println(i);
+		assertTrue(i>=0 && i<=3);
 	}
 	
 }
