cee21222fbf8cfd00bb7017dd7cc445a8d2437ed
Robert Hallink <rh222ff@student.lnu.se>
2014-11-13 22:05:04 +0100

    GREEN

--- Assignment 1/src/sticks/Player.java
+++ Assignment 1/src/sticks/Player.java
@@ -7,7 +7,23 @@ package sticks;
 public class Player {
 	
 	public Player() {
-		
+	}
+	
+	public int drawSticks() {
+		int number = -1;
+		while (number < 0) {
+			System.out.print("(Enter 0 to quit) How many sticks do you want to draw? Choose 1, 2 or 3: ");
+			try	{
+				number = (int) System.in.read();
+				if (number >= 48 && number <= 51)
+					number -= 48;
+				else number = -1;
+			}
+			catch(Exception e)	{ }
+			if (number < 0)
+				System.err.println("Error with input, try again please.");
+		}
+		return number;
 	}
 	
 }
