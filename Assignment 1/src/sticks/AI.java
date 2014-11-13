09a9397d6ff55be53bee5e738fce0c88acbd4729
Robert Hallink <rh222ff@student.lnu.se>
2014-11-13 22:26:30 +0100

    GREEN

--- Assignment 1/src/sticks/AI.java
+++ Assignment 1/src/sticks/AI.java
@@ -1,4 +1,5 @@
 package sticks;
+import java.util.Random;
 
 /*
  * TODO
@@ -6,8 +7,29 @@ package sticks;
  */
 public class AI extends Player {
 	
+	private Random random;
+	
 	public AI() {
-		
+		random = new Random();
 	}
 	
+	public int drawSticks(int max) {
+		if (max == 1 || max == 2) {
+			System.out.println("PC draws: 1");
+			return 1;
+		}
+		else if (max == 3) {
+			System.out.println("PC draws: 2");
+			return 2;
+		}
+		else if (max == 4) {
+			System.out.println("PC draws: 3");
+			return 3;
+		}
+		else {
+			int rand = random.nextInt(3) + 1;
+			System.out.println("PC draws: " + rand);
+			return rand;
+		}
+	}
 }
