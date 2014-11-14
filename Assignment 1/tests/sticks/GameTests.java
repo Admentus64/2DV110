66de9bdf95ad3bcb7db987fd312e4b7ee7122d99
Robert Hallink <rh222ff@student.lnu.se>
2014-11-14 17:00:03 +0100

    RED

--- Assignment 1/tests/sticks/GameTests.java
+++ Assignment 1/tests/sticks/GameTests.java
@@ -1,28 +1,58 @@
 package sticks;
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import java.util.Random;
 import org.junit.Test;
 
 public class GameTests {
 	
 	@Test
 	public void shouldCreateInstance() {
-		initGame();
+		initGame(false);
 	}
 	
 	@Test
-	public void shouldPlayGame() {
-		Game game = initGame();
-		try					{ while (game.play()); }
-		catch(Exception e)	{ fail("Failed test"); }
+	public void shouldPlayGameWithoutUserWithAnswerOne() {
+		Game game = initGame(true);
+		while (!game.isDone()) {
+			int answer = 1;
+			when(game.getPlayer().drawSticks(game.getStickList().unused())).thenReturn(answer);
+			System.out.println("Mock Player draws: " + answer);
+			game.play();
+		}
+	}
+	
+	@Test
+	public void shouldPlayGameWithoutUserWithRandomAnswer() {
+		Game game = initGame(true);
+		while (!game.isDone()) {
+			int answer = randomNumber(3, 1);
+			when(game.getPlayer().drawSticks(game.getStickList().unused())).thenReturn(answer);
+			System.out.println("Mock Player draws: " + answer);
+			game.play();
+		}
+	}
+	
+	@Test
+	public void shouldPlayGameWithUser() {
+		Game game = initGame(false);
+		while (!game.isDone())
+			game.play();
 	}
 	
 	
 	
-	private Game initGame() {
+	private Game initGame(boolean mock) {
 		StickList list = new StickList();
 		for (int i=0; i<30; i++)
 			list.add(new Stick());
-		return new Game(list, new Player(), new AI());
+		if (mock)
+			return new Game(list, mock(Player.class), new AI());
+		else return new Game(list, new Player(), new AI());
+	}
+	
+	private int randomNumber(int value, int add) {
+		Random rand = new Random();
+		return  rand.nextInt(value) + add;
 	}
 	
 }
