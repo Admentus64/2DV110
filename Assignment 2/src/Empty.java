cbfe609653f2ff8c1144e830f8b4972bdf5d5f96
Robert Hallink <rh222ff@student.lnu.se>
2014-12-16 14:17:54 +0100

    Uploaded Assignment 2

---
+++ Assignment 2/src/Empty.java
@@ -0,0 +1,13 @@
+/*
+ * This class is empty.
+ * No source code exists for this assignment.
+ */
+package assignment.pkg2;
+
+public class Empty {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
